4dce841c6a733e05c035a74bd613986abdef3e3b
Gleb Godonoga <gleb.godonoga@gmail.com>
2020-07-20 14:07:48 +0300

    Initial style and script setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  cacheDirectory: '../.jest',
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+      isolatedModules: true,
+      tsConfig: 'tsconfig.dev.json',
+    },
+  },
+  rootDir: 'test',
+  testEnvironment: 'node',
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+};
