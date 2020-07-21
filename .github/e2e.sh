924bac241c02fd0b926a1dadd3868511fecdd3be
Gleb Godonoga <gleb.godonoga@gmail.com>
2020-07-21 13:17:22 +0300

    Makefile setup with github actions

---
+++ .github/e2e.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+function finish() {
+  echo "Stopping $container"
+  docker stop "$container"
+}
+
+trap finish EXIT
+
+container=$(docker run -d "hiveadmin/article-hosting:${IMAGE_TAG:-local}")
+
+gtimeout --foreground 10 bash << EOT
+  while true; do
+    current=\$(docker inspect "${container}" | jq -r '.[0].State.Health.Status')
+    echo "${container} is in state: \${current}"
+    if [ "\$current" == "healthy" ]; then
+        break
+    fi
+    sleep 1
+  done
+EOT
