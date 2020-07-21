0732d71177b60dcbf86072e7ea725310371a4dcf
magrigor <maxim.agrigoroae@endava.com>
2020-07-21 12:36:11 +0300

    Add configuration file for app

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const config = {
+  server: {
+    port: process.env.APP_PORT || 8000,
+  }
+}
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import config from './config';
 import server from './server';
 
 server.on('listening', (): void => console.debug('debug', 'Server running'));
 
-server.listen(8000);
+server.listen(config.server.port);
