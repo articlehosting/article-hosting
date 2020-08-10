fc80639df55265876e57d7acf241a71987e76224
Alexandr Sisiuc <alexandr.sisiuc@endava.com>
2020-08-10 10:54:40 +0300

    Adds article author informations (#45)

--- src/components/article/article-info.ts
+++ src/components/article/article-info.ts
@@ -1,11 +1,11 @@
-import { Article, ArticleAuthor } from './article';
+import { Article, ArticleAuthor, ArticleDatePublished } from './article';
 import { renderContentBlock } from './article-content';
 import { renderDate } from '../../utils';
 
 export const renderAuthorDetails = (author: ArticleAuthor): string =>
   `<div>
     <h4 class="ui header">${author.givenNames.join(' ')} ${author.familyNames.join(' ')}</h4>
-    <div>${author.affiliations.map((affiliation) => `<span>${affiliation.name}, ${affiliation.address.addressLocality ?? ''}, ${affiliation.address.addressCountry}</span>`).join()}</div>
+    <div>${author.affiliations.map((affiliation) => `<span>${affiliation.name}, ${affiliation.address.addressLocality?.concat(', ') ?? ''}${affiliation.address.addressCountry}</span>`).join()}</div>
     <h5 class="ui header">For correspondence: <span>${author.emails.map((email) => `<a href="mailto:${email}">${email}</a>`).join(', ')}</span></h5>
     <div><a href="#">{orcid}</a></div>
 </div>`;
@@ -15,13 +15,13 @@ export const renderCopyright = (article: Article): string => {
 
   return `
     <h3>Copyright</h3>
-    <p>© ${new Date(article.datePublished.value).getFullYear()}, ${article.authors.length > 2 ? names.join(', ') : names.join(' and ')}</p>
+    <p>© ${new Date(article.datePublished.value).getFullYear()}, ${names.length > 2 ? names.join(', ') : names.join(' and ')}</p>
     <div>${article.licenses.map((license) => license.content.map((c) => renderContentBlock(c))).join()}</div>
   `;
 };
 
-export const renderVersion = (article: Article): string => {
-  const d = new Date(article.datePublished.value);
+export const renderVersion = (datePublished: ArticleDatePublished): string => {
+  const d = new Date(datePublished.value);
   return `<div>Version of Record published: <a href="#">${renderDate('mm dd, yy', 'long', 'four', d)} (version 1)</a></div>`;
 };
 
@@ -32,7 +32,7 @@ export const renderArticleInfo = (article: Article): string =>
       ${article.authors.map((author) => renderAuthorDetails(author)).join()}
     </div>
     <h3 class="ui header">Publication history</h3>
-    ${renderVersion(article)}
+    ${renderVersion(article.datePublished)}
     ${renderCopyright(article)}
   </div>
 `;
--- test/components/article/article-info.test.ts
+++ test/components/article/article-info.test.ts
@@ -1,6 +1,11 @@
 import article from '../../../src/__fixtures__/article';
 import { ArticleAuthor } from '../../../src/components/article/article';
-import { renderArticleInfo, renderAuthorDetails, renderCopyright } from '../../../src/components/article/article-info';
+import {
+  renderArticleInfo,
+  renderAuthorDetails,
+  renderCopyright,
+  renderVersion,
+} from '../../../src/components/article/article-info';
 
 describe('render article copyright', () => {
   it('should not throw', () => {
@@ -51,7 +56,23 @@ describe('render article copyright', () => {
       expect(renderAuthorDetails({
         ...person,
         affiliations,
-      })).toContain(`<span>${affiliations[0].name}, ${affiliations[0].address.addressLocality ?? ''}, ${affiliations[0].address.addressCountry}</span>`);
+      })).toContain(`<span>${affiliations[0].name}, ${affiliations[0].address.addressLocality}, ${affiliations[0].address.addressCountry}</span>`);
+    });
+
+    it('should renderAuthorDetails with affiliation and missing addressLocality', () => {
+      const affiliations = [{
+        type: 'Organization',
+        address: {
+          type: 'PostalAddress',
+          addressCountry: 'Uruguay',
+        },
+        name: 'Some organization',
+      }];
+
+      expect(renderAuthorDetails({
+        ...person,
+        affiliations,
+      })).toContain(`<span>${affiliations[0].name}, ${affiliations[0].address.addressCountry}</span>`);
     });
 
     it('should renderAuthorDetails with correspondence email', () => {
@@ -73,18 +94,106 @@ describe('render article copyright', () => {
       expect(renderCopyright(article)).toContain('<h3>Copyright</h3>');
     });
 
+    it('should renderCopyright with date and authors with single authors', () => {
+      expect(renderCopyright({
+        ...article,
+        authors: [{
+          type: 'Person',
+          affiliations: [
+            {
+              type: 'Organization',
+              address: {
+                type: 'PostalAddress',
+                addressCountry: 'Uruguay',
+                addressLocality: 'Montevideo',
+              },
+              name: 'Some organization',
+            },
+          ],
+          emails: [
+            'test@test.com',
+          ],
+          familyNames: [
+            'John',
+          ],
+          givenNames: [
+            'Smith',
+          ],
+        }],
+      })).toContain('<p>© 2020, John</p>');
+    });
+
     it('should renderCopyright with date and authors with 2 authors', () => {
-      expect(renderCopyright(article)).toContain('<p>© 2020, Bucheli and Olivieri</p>');
+      expect(renderCopyright({
+        ...article,
+        authors: article.authors,
+      })).toContain('<p>© 2020, Bucheli and Olivieri</p>');
+    });
+
+    it('should renderCopyright with date and authors with 3 authors', () => {
+      expect(renderCopyright({
+        ...article,
+        authors: [...article.authors, {
+          type: 'Person',
+          affiliations: [
+            {
+              type: 'Organization',
+              address: {
+                type: 'PostalAddress',
+                addressCountry: 'Uruguay',
+                addressLocality: 'Montevideo',
+              },
+              name: 'Some organization',
+            },
+          ],
+          emails: [
+            'test@test.com',
+          ],
+          familyNames: [
+            'John',
+          ],
+          givenNames: [
+            'Smith',
+          ],
+        }],
+      })).toContain('<p>© 2020, Bucheli, Olivieri, John</p>');
     });
 
     it('should renderCopyright with content', () => {
-      expect(renderCopyright(article)).toContain('<div><p>This article is distributed under the terms of the <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons');
+      expect(renderCopyright({
+        ...article,
+        licenses: [
+          {
+            type: 'CreativeWork',
+            url: 'http://creativecommons.org/licenses/by/4.0/',
+            content: [
+              {
+                type: 'Paragraph',
+                content: [
+                  'This article is distributed under the terms of the ',
+                  {
+                    type: 'Link',
+                    target: 'http://creativecommons.org/licenses/by/4.0/',
+                    content: [
+                      'Creative Commons Attribution License',
+                    ],
+                  },
+                  ', which permits unrestricted use and redistribution provided that the original author and source are credited.',
+                ],
+              },
+            ],
+          },
+        ],
+      })).toContain('<div><p>This article is distributed under the terms of the <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons');
     });
   });
 
   describe('should renderVersion in article', () => {
     it('should renderVersion with title', () => {
-      expect(renderCopyright(article)).toContain('<h3>Copyright</h3>');
+      expect(renderVersion({
+        type: 'Date',
+        value: '2020-07-17',
+      })).toContain('<div>Version of Record published: <a href="#">July 17, 2020 (version 1)</a></div>');
     });
   });
 });
