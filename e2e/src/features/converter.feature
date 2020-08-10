59e05648870370db7ca92737c96ba4baf42956a7
lmocan <ludmila.mocan@endava.com>
2020-08-10 16:27:37 +0300

    Add scenario for (#47) conversion of an invalid XML

--- e2e/src/features/converter.feature
+++ e2e/src/features/converter.feature
@@ -1,6 +1,11 @@
 Feature: Converter from XML to Json validation
 
-  Scenario: Load about page
-    Given the conversion endpoint
+  Scenario: Convert a valid XML to JSON
+    Given following XML file "ijm-00202.xml"
     When user calls the endpoint
     Then json is generated
+
+  Scenario: Convert an invalid XML
+    Given following XML file "ijmN-0020.xml"
+    When user calls the endpoint
+    Then json is not generated
--- e2e/src/step-definitions/httpSteps.js
+++ e2e/src/step-definitions/httpSteps.js
@@ -1,35 +1,40 @@
-import {Given} from 'cucumber';
+import {Given, When, Then} from 'cucumber';
 import chaiHttp from 'chai-http';
-import chai,{expect} from 'chai';
+import chai, {expect} from 'chai';
 import fs from "fs";
 import path from "path";
 import config from "../config";
 
-var {Then} = require('cucumber');
-
-var {When} = require('cucumber');
-
 chai.use(chaiHttp);
 
-Given(/^the conversion endpoint$/, async function () {
+Given(/^following XML file "([^"]*)"$/, async function (fileName) {
     const file = await new Promise((resolve, reject) =>
         fs.readFile(
-            path.join(config.filesDir, 'xml', 'ijm-00202.xml'),
+            path.join(config.filesDir, 'xml', fileName),
             (err, data) => err ? reject(err) : resolve(data)
         )
     );
+    this.data.fileXML.name = file;
 
+});
+
+When(/^user calls the endpoint$/, async function () {
     const res = await chai.request('http://localhost:8000')
         .post('/convert')
         .set('content-type', 'application/xml')
-        .send(file.toString());
-
-    expect(res).to.have.status(200);
-    expect(res).to.be.json;
+        .send(this.data.fileXML.name.toString());
+    this.data.result.value = res;
 });
-When(/^user calls the endpoint$/, function () {
 
-});
 Then(/^json is generated$/, function () {
+    const resp = this.data.result.value;
+    expect(resp).to.have.status(200);
+    expect(resp).to.be.json;
+    expect(resp.body).to.have.all.keys('title', 'authors', 'description', 'content', 'datePublished', 'isPartOf',
+        'identifiers', 'keywords', 'licenses', 'references', 'meta', 'type');
+});
 
+Then(/^json is not generated$/, function () {
+    const resp = this.data.result.value;
+    expect(resp).to.have.status(404);
 });
