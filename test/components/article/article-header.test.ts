9cad1631088d717b6c6d98473a58e4bb5202201d
magrigor <maxim.agrigoroae@endava.com>
2020-09-02 11:53:33 +0300

    Add link for downloading pdf on article page.

--- test/components/article/article-header.test.ts
+++ test/components/article/article-header.test.ts
@@ -61,9 +61,11 @@ describe('render article header', () => {
   });
 
   it('should not render article header with missing article', () => {
-    const wrongArticle = <Article><unknown>{};
+    const wrongArticle = <Article><unknown>null;
 
-    expect(() => renderArticleHeader(wrongArticle)).toThrow('Cannot read property \'map\' of undefined');
+    const result = renderArticleHeader(wrongArticle);
+
+    expect(result).toBe('');
   });
 
   it('should render article header in container', () => {
@@ -184,5 +186,49 @@ describe('render article header', () => {
       ...article,
       identifiers,
     })).toContain('<span>DOI: 1033.123321/ijm.123321</span>');
+  });
+
+  it('should render article header with article download pdf link', () => {
+    const identifiers = [
+      {
+        type: 'PropertyValue',
+        name: 'publisher-id',
+        propertyID: 'https://registry.identifiers.org/registry/publisher-id',
+        value: '001123',
+      },
+      {
+        type: 'PropertyValue',
+        name: 'doi',
+        propertyID: 'https://registry.identifiers.org/registry/doi',
+        value: '1033.123321/ijm.123321',
+      },
+    ];
+
+    expect(renderArticleHeader({
+      ...article,
+      identifiers,
+    })).toContain(`download/${identifiers[0].value}/ijm-${identifiers[0].value}.pdf`);
+  });
+
+  it('should render article header without article download pdf link', () => {
+    const identifiers = [
+      {
+        type: 'PropertyValue',
+        name: 'not-publisher-id',
+        propertyID: 'https://registry.identifiers.org/registry/publisher-id',
+        value: '001123',
+      },
+      {
+        type: 'PropertyValue',
+        name: 'doi',
+        propertyID: 'https://registry.identifiers.org/registry/doi',
+        value: '1033.123321/ijm.123321',
+      },
+    ];
+
+    expect(renderArticleHeader({
+      ...article,
+      identifiers,
+    })).not.toContain('Article PDF');
   });
 });
