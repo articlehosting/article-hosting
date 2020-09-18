2c500b3eb6c14d824f927698f9a98f49fbca8a56
Alexandr Sisiuc <alexandr.sisiuc@endava.com>
2020-09-18 17:09:57 +0300

    Figures page

--- src/components/article/article-figures.ts
+++ src/components/article/article-figures.ts
@@ -1,4 +1,6 @@
-import { Article, ArticleContents, ImageObjectContent } from './article';
+import {
+  Article, ArticleContents, ImageObjectContent, TableCellContent, TableContent, TableRowContent,
+} from './article';
 import { Context, renderImageObject } from './article-content';
 
 export const CONTENT_TABLE = 'Table';
@@ -15,8 +17,8 @@ export const renderContentBlock = (content?: ArticleContents | string, context?:
   }
 
   switch (content.type) {
-    // case CONTENT_TABLE:
-    //   return renderTable(<TableContent>content, context);
+    case CONTENT_TABLE:
+      return renderTable(<TableContent>content, context);
     case CONTENT_FIGURE:
       return renderImages(content, context);
     case CONTENT_IMAGEOBJECT:
@@ -29,6 +31,22 @@ export const renderContentBlock = (content?: ArticleContents | string, context?:
 export const renderContentArray = (content?: ArticleContents, context?: Context): string =>
   `${content?.content?.map((c) => renderContentBlock(c, context)).join('') ?? ''}`;
 
+export const renderTableRow = (content?: TableRowContent, context?: Context): string =>
+  `<tr>${content?.cells?.map((c) => renderTableCell(c, !!content?.rowType, context)).join('') ?? ''}</tr>`;
+
+export const renderTableCell = (content: TableCellContent, isHeader?: boolean, context?: Context): string =>
+  `<t${isHeader ? 'h' : 'd'} align='left'${content.rowSpan ? ` rowspan='${content.rowSpan}'` : ''}${content.colSpan ? ` colspan='${content.colSpan}'` : ''}>${renderContentArray(content, context)}</t${isHeader ? 'h' : 'd'}>`;
+
+export const renderTable = (content: TableContent, context?: Context): string =>
+  `<div${content.id ? ` id="${content.id}"` : ''}>
+    <span>${content.label}</span>${content.caption.map((c) => renderContentBlock(c, context)).join('')}
+     <table class="ui celled structured table">
+       <thead>${content.rows.map((row) => ((row.rowType && row.rowType === 'header') ? renderTableRow(row, context) : '')).join('')}</thead>
+       <tbody>${content.rows.map((row) => ((!row.rowType || (row.rowType && row.rowType !== 'header')) ? renderTableRow(row, context) : '')).join('')}</tbody>
+    </table>
+  </div>
+  `;
+
 export const renderImages = (content: ArticleContents, context?: Context): string =>
   `<div${content.id ? ` id="${content.id}"` : ''}>
     <div>
@@ -40,7 +58,15 @@ export const renderImages = (content: ArticleContents, context?: Context): strin
     </figure>
   </div>`;
 
-export const renderArticleFiguresContent = (article: Article): string => `
-${renderImages(article)}`;
+export const renderArticleFiguresContent = (article: Article): string => {
+  const renderContent = (type: string): string => `${article.content.filter((c) => c.type === type).map((contentBlock) => renderContentBlock(contentBlock, { article })).join('')}`;
+
+  return `<div class="ui container left aligned">
+    <h1>Figures.</h1>
+    ${renderContent(CONTENT_FIGURE)}
+    <h1>Tables.</h1>
+    ${renderContent(CONTENT_TABLE)}
+  </div>`;
+};
 
 export default renderArticleFiguresContent;
