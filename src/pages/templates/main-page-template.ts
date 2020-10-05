ca510e5e3aa7edc274d7277b72feead0b4db6484
cociugsergiu <sergiu.cociug@endava.com>
2020-10-05 13:52:50 +0300

    add and link css styles

--- src/pages/templates/main-page-template.ts
+++ src/pages/templates/main-page-template.ts
@@ -8,25 +8,7 @@ const mainPageTemplate = (pageContent: string): string => `
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
       <title>${config.name}</title>
-      <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"/>
-      <style type="text/css">
-        body {
-          background-color: #FFFFFF;
-        }
-        .ui.menu .item img.logo {
-          margin-right: 1.5em;
-        }
-        .main.container {
-          margin-top: 7em;
-        }
-        .wireframe {
-          margin-top: 2em;
-        }
-        .ui.footer.segment {
-          margin: 5em 0em 0em;
-          padding: 5em 0em;
-        }
-      </style>
+      <link type="text/css" rel="stylesheet" href="/css/style.css"/>
     </head>
     <body>
         ${renderMainHeader()}
