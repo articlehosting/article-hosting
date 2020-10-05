febb60b8236be63d8ce36b9898730f286269163a
gaenache <gabriela.enache@endava.com>
2020-10-05 16:02:34 +0300

    Refactor in step definition

--- e2e/src/config/pages.js
+++ e2e/src/config/pages.js
@@ -1,8 +1,7 @@
 export default {
-
     "Article": "articles",
-    "Figures":"figures",
-    "Mendeley":"mendeley&redirect_uri=https%3A%2F%2Fwww.mendeley.com%2Fcallback%2F&client_id=MENDELEY",
-    "First Article":"The dynamic cross-sectional microsimulation model MOSART",
-    "View":"iiif",
+    "Figures": "figures",
+    "Mendeley": "mendeley&redirect_uri=https%3A%2F%2Fwww.mendeley.com%2Fcallback%2F&client_id=MENDELEY",
+    "First Article": "Canonical Wnt-signaling modulates the tempo of dendritic growth of adult-born hippocampal neurons",
+    "View": "iiif",
 }
