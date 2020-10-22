94d58db1ac8e2f6cae5985181a8cb83fecbc3170
lmocan <ludmila.mocan@endava.com>
2020-10-22 16:34:14 +0300

    Added steps for validation of metadata, back-matter, files Hypermedia endpoints

--- e2e/src/features/api.feature
+++ e2e/src/features/api.feature
@@ -1,40 +1,49 @@
 Feature: Hypermedia API scenarios
 
   Scenario: Entry point is displayed
-    Given endpoint "rdf" with parameters
+    Given endpoint "rdf"
     When the request is send
     Then the context is returned
 
   Scenario: All articles endpoint
-    Given endpoint "rdf/articles" with parameters
+    Given endpoint "rdf/articles"
     When the request is send
     Then the list of articles is returned
 
-  Scenario: Metadata for an article
-    Given endpoint "" with parameters
+
+  Scenario Outline: Metadata for an article
+    Given endpoint "/metadata" with parameters
+      | <id> |
     When the request is send
     Then metada of article is returned
+    Examples:
+      | id                        |
+      | 10.1101/2020.01.06.895854 |
 
   Scenario: Article body endpoint
     Given endpoint "" with parameters
-    ||
+      |  |
     When the request is send
     Then article body is returned
 
-  Scenario: Article back matter endpoint
-    Given endpoint "" with parameters
-    ||
+
+  Scenario Outline: Article back matter endpoint
+    Given endpoint "/back-matter" with parameters
+      | <id> |
     When the request is send
     Then article back matter is returned
+    Examples:
+      | id                        |
+      | 10.1101/2020.01.06.895854 |
 
   Scenario: Downloading article related materials endpoint
-    Given endpoint "" with parameters
-      ||
+    Given endpoint "/files" with parameters
+      | 10.1101/2020.01.06.895854 |
     When the request is send
     Then related materials are downloaded
 
   Scenario: Retrieve images endpoint
     Given endpoint "" with parameters
-      ||
+      |  |
     When the request is send
     Then list of images is returned
--- e2e/src/step-definitions/httpSteps.js
+++ e2e/src/step-definitions/httpSteps.js
@@ -22,18 +22,27 @@ Given(/^following XML file "([^"]*)"$/, async function (fileName) {
 
 });
 
-Given(/^endpoint "([^"]*)" with parameters$/, async function (endpoint) {
+Given(/^endpoint "([^"]*)"$/, async function (endpoint) {
     this.data = {
         endpoint: {
             name: endpoint
         }
     };
-    // for(const param of list.rawTable.flat()){
-    //     this.data={
-    //         listOfParams:{
-    //             param}
-    //     };
-    // }
+});
+
+Given(/^endpoint "([^"]*)" with parameters$/, async function (entrypoint, params) {
+    for (const param of params.rawTable.flat()) {
+        const id = param;
+
+        const endpoint = `rdf/articles/${id}${entrypoint}`
+        this.data = {
+            endpoint: {
+                name: endpoint
+            }
+        };
+    }
+    ;
+
 });
 
 When(/^the request is send$/, async function () {
@@ -41,7 +50,6 @@ When(/^the request is send$/, async function () {
     const res = await chai.request(config.url)
         .get(`/${endpoint}`)
         .set('content-type', 'application/xml')
-    // .send(this.data.listOfParams);
     this.data = {
         result: {
             value: res
@@ -77,8 +85,21 @@ Then(/^json is not generated$/, function () {
 Then(/^metada of article is returned$/, function () {
     const resp = this.data.result.value;
     expect(resp).to.have.status(200);
-    // console.log("Response",resp.body["@id"]);
-    console.log("Response", resp.toString());
+    const graph = resp.body["@graph"];
+    if (graph.length > 0) {
+        for (let i = 0; i <= graph[graph.length - 1]; i += 1) {
+            expect(graph[i]).to.have.keys("@id", "schema:title", "schema:about", "schema:author",
+                "schema:dateAccepted","schema:datePublished","schema:dateReceived","schema:isPartOf");
+            expect(graph[i]["schema:title"]).to.have.keys("@value");
+            expect(graph[i]["schema:about"]).to.have.keys("@id");
+            expect(graph[i]["schema:author"]).to.have.keys("@id");
+            expect(graph[i]["schema:dateAccepted"]).to.have.keys("@id");
+            expect(graph[i]["schema:datePublished"]).to.have.keys("@id");
+            expect(graph[i]["schema:dateReceived"]).to.have.keys("@id");
+            expect(graph[i]["schema:isPartOf"]).to.have.keys("@id");
+            expect(graph[i]["@id"]).to.contain("_:b");
+        }
+    }
 });
 
 Then(/^the list of articles is returned$/, function () {
@@ -95,11 +116,33 @@ Then(/^the list of articles is returned$/, function () {
         }
     }
     const nodeRoute = graph[graph.length - 1];
-    expect(nodeRoute).to.have.all.keys("@id", "@type", "schema:Article", "schema:name");
+    expect(nodeRoute).to.have.all.keys("@id", "@type", "schema:Article","hydra:manages" ,"schema:name");
     expect(nodeRoute["@id"]).to.contain("http://article.hosting/rdf/articles");
     expect(nodeRoute["@type"]).to.contain("schema:WebAPI");
     expect(nodeRoute["schema:Article"]).to.be.an('array');
     expect(nodeRoute["schema:name"]).to.contain('Article Hosting RDF Graph: List Articles');
+    expect(nodeRoute["hydra:manages"]).to.have.keys("@id");
+});
+
+Then(/^article back matter is returned$/, function () {
+    const resp = this.data.result.value;
+    expect(resp).to.have.status(200);
+    const graph = resp.body["@graph"];
+    if (graph.length > 0) {
+        for (let i = 0; i <= graph[graph.length - 1]; i += 1) {
+            expect(graph[i]).to.have.all.keys("@id", "schema:about", "schema:author", "schema:citation", "schema:dateAccepted",
+                "schema:datePublished","schema:dateReceived","schema:file","schema:identifier","schema:license");
+            expect(graph[i]["schema:about"]).to.have.keys("@id");
+            expect(graph[i]["schema:citation"]).to.have.keys("@id");
+            expect(graph[i]["schema:dateAccepted"]).to.have.keys("@id");
+            expect(graph[i]["schema:datePublished"]).to.have.keys("@id");
+            expect(graph[i]["schema:dateReceived"]).to.have.keys("@id");
+            expect(graph[i]["schema:file"]).to.have.keys("@id");
+            expect(graph[i]["schema:identifier"]).to.have.keys("@id");
+            expect(graph[i]["schema:license"]).to.have.keys("@id");
+            expect(graph[i]["@id"]).to.contain("_:b");
+        }
+    }
 });
 
 Then(/^the context is returned$/, function () {
@@ -110,4 +153,18 @@ Then(/^the context is returned$/, function () {
     const endpoint = graph[0]["@id"];
     expect(endpoint).to.be.equal("http://article.hosting/rdf");
     expect(graph[1]["@id"]).to.be.equal("http://article.hosting/rdf/articles");
+});
+Then(/^related materials are downloaded$/, function () {
+    const resp = this.data.result.value;
+    expect(resp).to.have.status(200);
+    const graph = resp.body["@graph"];
+    if (graph.length > 0) {
+        for (let i = 0; i <= graph[graph.length - 1]; i += 1) {
+            expect(graph[i]).to.have.all.keys("@id", "schema:contentUrl", "hydra:Link", "schema:fileExtension");
+            expect(graph[i]["schema:contentUrl"]).to.have.keys("@value");
+            expect(graph[i]["hydra:Link"]).to.have.keys("@id");
+            expect(graph[i]["'schema:fileExtension"]).to.have.keys("@value");
+            expect(graph[i]["@id"]).to.contain("_:b");
+        }
+    }
 });
