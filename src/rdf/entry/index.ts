a1556cdbedb679320ac5663f1a555529343ba855
magrigor <maxim.agrigoroae@endava.com>
2020-10-23 15:29:39 +0300

    Repair hypermedia entry point content

--- src/rdf/entry/index.ts
+++ src/rdf/entry/index.ts
@@ -1,22 +1,20 @@
+import { literal } from '@rdfjs/data-model';
 import { AnyPointer } from 'clownface';
 import { NamedNode } from 'rdf-js';
 import config from '../../config';
 import routes from '../../config/routes';
 import { AppContext } from '../../server/context';
 import { createNamedNode } from '../../server/data-factory';
-import { hydra, rdf, schema } from '../namespaces';
+import {
+  hydra, rdf, schema,
+} from '../namespaces';
 
 export const entryHandler = async (graph: AnyPointer<NamedNode, any>, ctx: AppContext): Promise<void> => {
   graph.addOut(rdf.type, schema.EntryPoint);
-  graph.addOut(schema('name'), ctx.dataFactory.literal('Article Hosting RDF Graph', config.rdf.Language));
+  graph.addOut(schema('name'), literal(config.name, config.rdf.Language));
 
-  graph.addOut(hydra.collection, createNamedNode(ctx.router, ctx.request, routes.rdf.Articles), (linkNode) => {
-    linkNode.addOut(rdf.type, hydra.Collection);
-    linkNode.addOut(hydra.title, 'List of RDF Articles');
-  });
-
-  graph.addOut(hydra.Link, createNamedNode(ctx.router, ctx.request, routes.pages.HomePage), (homePageNode) => {
-    homePageNode.addOut(hydra.title, 'List articles HTML Home page');
+  graph.addOut(hydra.collection, createNamedNode(ctx.router, ctx.request, routes.rdf.Articles), (list): void => {
+    list.addOut(rdf.type, hydra.Collection);
   });
 };
 
