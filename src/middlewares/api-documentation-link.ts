f0f58428f9ad63d575bc054ec5d78528f9874935
magrigor <maxim.agrigoroae@endava.com>
2020-10-28 16:44:11 +0200

    Add exerimental switch of http to https for apidoc

--- src/middlewares/api-documentation-link.ts
+++ src/middlewares/api-documentation-link.ts
@@ -7,9 +7,16 @@ export default (path: string): Middleware<ExtendableContext> => (
   async ({ request, response }: ExtendableContext, next: Next): Promise<void> => {
     await next();
 
+    let apiDocLink = url.resolve(request.origin, path);
+
+    // @todo: rework it..
+    if (process.env.NODE_ENV === 'production' && !/^https/.test(apiDocLink)) {
+      apiDocLink = apiDocLink.replace(/^http/, 'https');
+    }
+
     const link = {
       rel: 'http://www.w3.org/ns/hydra/core#apiDocumentation',
-      url: url.resolve(request.origin, path),
+      url: apiDocLink,
     };
 
     response.append('Link', formatLinkHeader({ hydra: link }));
