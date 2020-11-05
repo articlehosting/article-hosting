24fc65ffa3907ac6c63dde6e72d62bd9edde9905
magrigor <maxim.agrigoroae@endava.com>
2020-11-05 15:42:05 +0200

    Remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,4 @@ const { port } = config.server;
 server.on('listening', (): void => console.debug('debug', `Server running on port ${port}`));
 server.listen(port);
 
-// if (process.env.NODE_ENV === 'production') {
-//   const httpsPort = config.server.https.port;
-//
-//   const httpsServer = createHttpsServer(config.server.https.options, app.callback());
-//
-//   httpsServer.on('listening', (): void => console.debug('debug', `HTTPS Server running on port ${httpsPort}`));
-//
-//   httpsServer.listen(httpsPort);
-// }
-
 export default server;
